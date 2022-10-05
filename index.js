6afde81f762d5d7c7b5ae9483fb266af9d7e480c
rmrmrmr <ricardomartineza77@gmail.com>
2022-10-05 14:02:58 -0500

    Fix indentation for loadBooks.js file

--- index.js
+++ index.js
@@ -3,46 +3,14 @@ import { displayList, displayAddNew, displayContact } from './modules/functions.
 import {
   form, navList, navAddNew, navContact, listBooks,
 } from './modules/selectors.js';
+import { loadBooks } from './modules/loadBooks.js';
 
 const eventListeners = () => {
   // add book
   form.addEventListener('submit', addBook);
 
   // book DOM
-  document.addEventListener('DOMContentLoaded', () => {
-    const data = JSON.parse(localStorage.getItem('books')) || [];
-
-    for (let i = 0; i < data.length; i += 1) {
-      const bookContainer = document.createElement('div');
-      bookContainer.classList.add('book-wrap');
-      listBooks.appendChild(bookContainer);
-
-      const wrap = document.createElement('div');
-      wrap.classList.add('wrap');
-      bookContainer.appendChild(wrap);
-
-      const tittleBook = document.createElement('p');
-      tittleBook.classList.add('tittle-book');
-      tittleBook.innerText = `"${data[i].tittle}"`;
-      wrap.appendChild(tittleBook);
-
-      const authorBook = document.createElement('p');
-      authorBook.classList.add('author-book');
-      authorBook.innerText = `by ${data[i].author}`;
-      wrap.appendChild(authorBook);
-
-      const removeBtn = document.createElement('button');
-      removeBtn.classList.add('btn-remove');
-      removeBtn.innerText = 'Remove';
-      bookContainer.appendChild(removeBtn);
-
-      removeBtn.addEventListener('click', (parameter) => {
-        if (parameter.target.classList.value === 'btn-remove') {
-          listBooks.removeChild(parameter.target.parentElement);
-        }
-      });
-    }
-  });
+  document.addEventListener('DOMContentLoaded', loadBooks);
 
   // display sections
   navList.addEventListener('click', displayList);
