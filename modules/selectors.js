4f078e272ebb26a1cbc5529eec40c3e8d95458e2
rmrmrmr <ricardomartineza77@gmail.com>
2022-10-05 11:48:32 -0500

    Add JS modules functionality

---
+++ modules/selectors.js
@@ -0,0 +1,10 @@
+export const form = document.getElementById('booksForm');
+export const listBooks = document.getElementById('booksList');
+
+export const list = document.getElementById('list');
+export const addNew = document.getElementById('addNew');
+export const contact = document.getElementById('contact');
+
+export const navList = document.getElementById('navList');
+export const navAddNew = document.getElementById('navAddNew');
+export const navContact = document.getElementById('navContact');
